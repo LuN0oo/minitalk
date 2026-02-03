235ad72701c9f911f4c8965cfb4291a392aceb96
Antonin Alaphilippe <analaphi@1E6.42angouleme.fr>
2026-02-03 10:33:52 +0100

    added minitalk.h and updated client.c

--- client.c
+++ client.c
@@ -5,8 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: analaphi <analaphi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/02 15:30:59 by analaphi          #+#    #+#             */
-/*   Updated: 2026/02/02 15:31:00 by analaphi         ###   ########.fr       */
+/*   Created: 2026/01/30 14:35:25 by analaphi          #+#    #+#             */
+/*   Updated: 2026/02/03 10:33:31 by analaphi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+int	send_byte(unsigned char c, pid_t pid)
+{
+	
+}
+
+int	send_string(char *str, pid_t pid)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		send_byte((unsigned char)str[i], pid);
+		i++;
+	}
+	send_byte(0, pid);
+}
+
+int	main(int ac, char **av)
+{
+	
+}
---
+++ minitalk.h
@@ -0,0 +1,10 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <sched.h>
+
+size_t	ft_strlen(const char *str);
+
+#endif
